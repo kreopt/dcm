c8911a03de45622e52ea2f00f3dccbc53ede6c02
kreopt_wf34 <kreopt@gmail.com>
2015-03-25 16:01:10 +0600

    * interactive sender

--- sender.cpp
+++ sender.cpp
@@ -10,6 +10,7 @@
 #include <fstream>
 #include <core/message.hpp>
 
+#include "json11.hpp"
 #include "socket/dcm_socket_client.hpp"
 
 using asio::ip::tcp;
@@ -19,10 +20,10 @@ int main(int argc, char *argv[]) {
     try {
         // II. Start sender
         // Check command line parameters
-        /*if (argc != 3) {
-            std::cerr << "Usage: Client <host> <port>" << std::endl;
+        if (argc < 1) {
+            std::cerr << "Usage: sender <unix_socket>" << std::endl;
             return 1;
-        }*/
+        }
 
         // Create sender object
 //        auto io_service = std::make_shared<asio::io_service>();
@@ -32,14 +33,37 @@ int main(int argc, char *argv[]) {
 //        tcp_client sender(*io_service, iterator);
 //        std::thread client_thread([&io_service](){io_service->run();});
 
-        auto sender = dcm::streamsocket::make_sender(interproc::streamsocket_type::unix, "unix_server.sock");
+        auto sender = dcm::streamsocket::make_sender(interproc::streamsocket_type::unix, argv[1]);
         sender->connect();
 
         int i = 0;
         while (true) {
+            std::string sig_name, json_data;
+            std::cout << "signal: ";
+            std::getline(std::cin, sig_name);
+            if (sig_name.empty()) {
+                break;
+            }
+            std::cout << std::endl << "data in json: ";
+            std::getline(std::cin, json_data);
+
             dcm::message message;
-            message.header["signal"] = "dcm.sample.signal";
-            message.body["data"] = "sample data " + std::to_string(i);
+            message.header["signal"] = sig_name;
+            try {
+                std::string err;
+                json11::Json json = json11::Json::parse(json_data, err);
+                if (!err.empty()) {
+                    std::cerr << err << std::endl;
+                    continue;
+                }
+
+                for (auto &e: json.object_items()) {
+                    message.body[e.first] = e.second.string_value();
+                }
+            } catch(...){
+                std::cerr << "bad input" << std::endl;
+                continue;
+            }
 
             sender->send(message);
             std::this_thread::sleep_for(std::chrono::seconds(1));
