026c93677e17927674c7337783f8b018ac2b5cbc
kreopt_wf34 <kreopt@gmail.com>
2015-05-05 15:03:30 +0600

    * fix signal read

--- src/core/message.cpp
+++ src/core/message.cpp
@@ -5,104 +5,6 @@
 #include <iostream>
 #include "message.hpp"
 
-
-/*dcm::message::message(const interproc::buffer &_encoded) {
-    decode(_encoded);
-}
-
-dcm::message::message(interproc::buffer &&_encoded) {
-    decode(_encoded);
-}
-
-dcm::message::message() {
-
-}
-
-interproc::buffer dcm::message::encode_block(const dcm::message::block_t &_block) const {
-    interproc::obufstream ba;
-    interproc::write_size(ba, 0);
-    interproc::block_size_t block_size = 0;
-    for (auto &b: _block){
-        interproc::write_size(ba, b.first.size());
-        ba << b.first;
-        interproc::write_size(ba, b.second.size());
-        ba << b.second;
-        block_size += b.first.size()+b.second.size()+2*sizeof(interproc::block_size_t);
-    }
-    auto pos = ba.tellp();
-    ba.seekp(0);
-    interproc::write_size(ba, block_size);
-    ba.seekp(pos);
-    return ba.str();
-}
-
-interproc::buffer dcm::message::encode() const {
-    return encode_block(header)+encode_block(body);
-}
-
-void dcm::message::decode_header(const interproc::buffer &_encoded) {
-    decode_block(_encoded, header);
-}
-
-void dcm::message::decode_body(const interproc::buffer &_encoded) {
-    decode_block(_encoded, body);
-}
-
-void dcm::message::decode_block(const interproc::buffer &_buf, std::unordered_map<std::string, interproc::buffer> &_container) {
-    std::string key;
-    interproc::buffer val;
-    interproc::ibufstream bs(_buf);
-    interproc::block_size_t len;
-    int pos = 0;
-    while (pos < _buf.size()){
-        interproc::read_size(bs, len);
-        key.resize(len);
-        bs.read(&key[0], len);
-        pos+=len;
-
-        interproc::read_size(bs, len);
-        val.resize(len);
-        bs.read(&val[0], len);
-        pos+=len;
-
-        _container[key] = val;
-        pos+=2*interproc::BLOCK_SIZE_SIZE;
-    }
-}
-
-void dcm::message::decode(const interproc::buffer &_encoded) {
-    interproc::ibufstream bs(_encoded);
-    interproc::block_size_t len = 0;
-    interproc::read_size(bs, len);
-    decode_header(interproc::buffer(_encoded, interproc::BLOCK_SIZE_SIZE, len));
-
-    interproc::ibufstream bbs(interproc::buffer(_encoded, interproc::BLOCK_SIZE_SIZE+len));
-    interproc::block_size_t len1 = 0;
-    interproc::read_size(bbs, len1);
-    decode_body(interproc::buffer(_encoded.begin()+2*interproc::BLOCK_SIZE_SIZE+len, _encoded.end()));
-}
-
-dcm::message::message(dcm::message &&_message) {
-    this->header = std::move(_message.header);
-    _message.header.clear();
-    this->body = std::move(_message.body);
-    _message.body.clear();
-}
-
-interproc::buffer dcm::message::encode_header() const {
-    return encode_block(header);
-}
-
-interproc::buffer dcm::message::encode_body() const {
-    return encode_block(body);
-}
-
-dcm::message::message(dcm::message::block_t &&_header, dcm::message::block_t &&_body) {
-    header = _header;
-    body = _body;
-}
-*/
-
 const interproc::buffer dcm::signal::encode_block(const dcm::signal::message_block_t &_block) {
     interproc::obufstream ba;
     interproc::write_size(ba, 0);
@@ -184,26 +86,23 @@ interproc::buffer dcm::signal::encode() const {
 
 dcm::signal dcm::signal::decode(const interproc::buffer &_buf) {
     dcm::signal s;
-    interproc::ibufstream bs0(_buf);
-    interproc::ibufstream bs(interproc::buffer(_buf, interproc::BLOCK_SIZE_SIZE));
-    interproc::block_size_t pkg_len = 0;
+    interproc::ibufstream bs(_buf);
     interproc::block_size_t len = 0;
-    interproc::read_size(bs0, pkg_len);
     interproc::read_size(bs, len);
 
-    if (len > pkg_len || pkg_len != _buf.length()-interproc::BLOCK_SIZE_SIZE) {
-        throw decode_error("bad block length: pkg_len="+std::to_string(pkg_len)+"; head_len="+std::to_string(len)+"; real_len="+std::to_string(_buf.length()));
+    if (len > _buf.length()-interproc::BLOCK_SIZE_SIZE) {
+        throw decode_error("bad block length: head_len="+std::to_string(len)+"; real_len="+std::to_string(_buf.length()));
     }
-    s.header_ = decode_block(interproc::buffer(_buf, 2*interproc::BLOCK_SIZE_SIZE, len));
+    s.header_ = decode_block(interproc::buffer(_buf, interproc::BLOCK_SIZE_SIZE, len));
 
-    interproc::ibufstream bbs(interproc::buffer(_buf, 2*interproc::BLOCK_SIZE_SIZE+len));
+    interproc::ibufstream bbs(interproc::buffer(_buf, interproc::BLOCK_SIZE_SIZE+len));
     interproc::block_size_t len1 = 0;
     interproc::read_size(bbs, len1);
 
-    if (len+len1 > pkg_len) {
-        throw decode_error("bad block length: pkg_len="+std::to_string(pkg_len)+"; head_len="+std::to_string(len)+"; body_len="+std::to_string(len1)+"; real_len="+std::to_string(_buf.length()));
+    if (len+len1 > _buf.length()-2*interproc::BLOCK_SIZE_SIZE) {
+        throw decode_error("bad block length: head_len="+std::to_string(len)+"; body_len="+std::to_string(len1)+"; real_len="+std::to_string(_buf.length()));
     }
-    s.body_ = decode_block(interproc::buffer(_buf.begin()+3*interproc::BLOCK_SIZE_SIZE+len, _buf.end()));
+    s.body_ = decode_block(interproc::buffer(_buf.begin()+2*interproc::BLOCK_SIZE_SIZE+len, _buf.end()));
     return s;
 }
 
--- src/socket/dcm_socket_server.hpp
+++ src/socket/dcm_socket_server.hpp
@@ -2,6 +2,7 @@
 #define _DCM_DCM_SOCKET_SERVER_HPP_
 
 #include <dcm/interprocess/streamsocket/listener.hpp>
+#include <dcm/interprocess/streamsocket/listener_session.hpp>
 #include <mutex>
 #include "../core/message.hpp"
 
@@ -13,6 +14,36 @@ namespace dcm {
             virtual std::shared_ptr<dcm_receiver_t> on(const std::string &_signal, std::function<void(const dcm::signal&)> _handler) = 0;
             virtual std::shared_ptr<dcm_receiver_t> off(const std::string &_signal) = 0;
             virtual std::shared_ptr<dcm_receiver_t> fail(std::function<void(std::exception &e)>) = 0;
+        };
+
+        template<typename protocol_type, typename buffer_type = interproc::buffer>
+        class session : public interproc::streamsocket::listener_session<protocol_type>{
+            bool is_size_;
+        public:
+            explicit session(asio::io_service &io_service) :
+                interproc::streamsocket::listener_session<protocol_type>(io_service) {
+                is_size_ = true;
+            }
+
+            virtual void read() {
+                // Check input buffer
+                this->reader_->read(interproc::BLOCK_SIZE_SIZE);
+            }
+
+            virtual void read_success_handler(interproc::buffer &&_buffer) {
+                interproc::block_size_t sz;
+                if (is_size_) {
+                    interproc::ibufstream bs(_buffer);
+                    interproc::read_size(bs, sz);
+                } else {
+                    sz = interproc::BLOCK_SIZE_SIZE;
+                    if (this->on_message) this->on_message(_buffer);
+                }
+                is_size_=!is_size_;
+                if (!this->eof_) {
+                    this->reader_->read(sz);
+                }
+            }
         };
 
         // tcp_server class
@@ -27,7 +58,7 @@ namespace dcm {
         public:
             // Constructor
             explicit stream_socket_receiver(const std::string &_endpoint) :
-                    receiver_(std::make_shared<interproc::streamsocket::listener_impl<protocol_type>>(_endpoint)) {
+                    receiver_(std::make_shared<interproc::streamsocket::listener_impl<protocol_type, interproc::buffer, session>>(_endpoint)) {
                 receiver_->on_message = [this](const interproc::buffer &_buf){
                     if (this->on_message) {
                         this->on_message(dcm::signal::decode(_buf));
